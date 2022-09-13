9e8009ef0a20fab0f949ba2ad448a83eb164e69a
liulei <liuleiytu@gmail.com>
2022-09-14 03:11:31 +0800

    fix(runtime): runtime

--- websites/runtime/src/App.tsx
+++ websites/runtime/src/App.tsx
@@ -1,13 +1,10 @@
 import { FC } from 'react'
 import './App.css'
-import refine from '@modou/refine'
 
 const App: FC = () => {
   return (
     <div className="App">
-      <button onClick={() => {
-        console.log(refine.object({}))
-      }}>测试
+      <button onClick={() => {}}>测试
       </button>
     </div>
   )
