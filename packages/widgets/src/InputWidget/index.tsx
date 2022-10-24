a013597c4f99ea1001f2f094b09bcc1659e6b68c
liulei <liuleiytu@gmail.com>
2022-10-25 01:41:01 +0800

    fix(widgets): iframe内部input无法选中的问题

--- packages/widgets/src/InputWidget/index.tsx
+++ packages/widgets/src/InputWidget/index.tsx
@@ -10,17 +10,17 @@ export const InputWidget: FC<
   useEffect(() => {
     console.log('我是Input 我重新渲染了')
   })
-  useEffect(() => {
-    const hackElement = document.querySelector(
-      `[data-hack-widget-id=${instance.widgetId}]`,
-    )
-    hackElement?.parentElement?.setAttribute(
-      'data-widget-id',
-      instance.widgetId,
-    )
-  }, [instance.widgetId])
+  // useEffect(() => {
+  //   const hackElement = document.querySelector(
+  //     `[data-hack-widget-id=${instance.widgetId}]`,
+  //   )
+  //   hackElement?.parentElement?.setAttribute(
+  //     'data-widget-id',
+  //     instance.widgetId,
+  //   )
+  // }, [instance.widgetId])
   return (
-    <Form.Item data-hack-widget-id={instance.widgetId} label={label}>
+    <Form.Item data-widget-id={instance.widgetId} label={label}>
       <Input
         defaultValue={defaultValue}
         value={value}
