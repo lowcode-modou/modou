358df749d6c6cafae2b4067a5ba35a0ade7f806b
liulei <liuleiytu@gmail.com>
2022-12-17 13:40:36 +0800

    refactor(code-editor): 删除无用代码

--- auxiliaries/code-editor/src/CodeEditor/index.tsx
+++ auxiliaries/code-editor/src/CodeEditor/index.tsx
@@ -1,20 +1,75 @@
-import React, { type FC, useRef } from 'react'
+import CodeMirror from 'codemirror'
+import React, { type FC, useEffect, useRef } from 'react'
 
+import {
+  CodeEditorModeEnum,
+  CodeEditorThemeEnum,
+  CodeEditorThemes,
+} from '@modou/code-editor/CodeEditor/editor-config'
 import { mcss } from '@modou/css-in-js'
 
 import './code-mirror-libs'
 import './modes'
 
 export const CodeEditor: FC<{}> = (props) => {
-  const editorTarget = useRef<HTMLDivElement>(null)
+  const cmWrapperRef = useRef<HTMLDivElement>(null)
+  const cmRef = useRef<CodeMirror.Editor | null>(null)
+  useEffect(() => {
+    if (!cmWrapperRef.current) {
+      return
+    }
+    if (!cmRef.current) {
+      cmRef.current = CodeMirror(cmWrapperRef.current, {
+        value: `{{const name = '小明'}}`,
+        mode: CodeEditorModeEnum.TextWithJs,
+        tabindex: 1,
+        lineWrapping: true,
+        theme: CodeEditorThemes[CodeEditorThemeEnum.Light],
+        viewportMargin: Infinity,
+        hintOptions: {
+          completeSingle: false,
+        },
+        autoRefresh: {
+          delay: 50,
+        },
+      })
+    }
+  }, [])
   return (
-    <div className={editorWrapperStyle}>
-      <div className="CodeEditorTarget" tabIndex={0} ref={editorTarget}>
-        CodeEditor
-      </div>
+    <div className={classes.wrapper}>
+      <div className={classes.cmWrapper} tabIndex={0} ref={cmWrapperRef} />
     </div>
   )
 }
 
-const editorWrapperStyle = mcss`
-`
+const classes = {
+  wrapper: mcss`
+		border: 1px solid red;
+  `,
+  cmWrapper: mcss`
+    height: 100%;
+		width: 100%;
+    overflow: hidden;
+    .CodeMirror {
+       width: 100%;
+       height: 100px;
+       padding: 0;
+       border-radius: var(--chakra-radii-sm);
+       background: var(--chakra-colors-gray-100);
+       color: var(--chakra-colors-gray-800);
+       transition-property: var(--chakra-transition-property-common);
+       transition-duration: var(--chakra-transition-duration-normal);
+       &:hover {
+         background: var(--chakra-colors-gray-200);
+       }
+ 
+       .cm-sunmao-ui {
+         background: var(--chakra-colors-green-100);
+       }
+    }
+    .CodeMirror .CodeMirror-scroll {
+          max-height: 125px
+};
+    
+  `,
+}
